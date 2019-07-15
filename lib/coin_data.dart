ae684a36554bde83db5bde90fefc1745afe93df9
Vignesh M <ms.vignesh31@gmail.com>
2019-07-15 09:35:15 +0530

    Finished the bitcoin ticker using Flutter, dart and
    http and dart:convert

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,20 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future getPrice(String fiat, String crypto) async {
+    String currencyId = fiat.toUpperCase();
+    String cryptoId = crypto.toUpperCase();
+    final response = await http.get(
+        'https://apiv2.bitcoinaverage.com/indices/global/ticker/$cryptoId$currencyId');
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON.
+      var res = jsonDecode(response.body);
+      print(res);
+      return res['last'];
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'coin_data.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +9,71 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  String bitCoinPrice = '?';
+  String ethreumPrice = '?';
+  String lightCoinPrice = '?';
+
+  void getData(String currency) async {
+    CoinData data = CoinData();
+    double bitCoinlast = await data.getPrice(currency, 'BTC');
+    double ethreumlast = await data.getPrice(currency, 'ETH');
+    double lightCoinlast = await data.getPrice(currency, 'LTC');
+    setState(() {
+      this.bitCoinPrice = bitCoinlast.toStringAsFixed(2);
+      this.ethreumPrice = ethreumlast.toStringAsFixed(2);
+      this.lightCoinPrice = lightCoinlast.toStringAsFixed(2);
+    });
+  }
+
+  CupertinoPicker iOSPicker() {
+    List<Text> menu = [];
+    currenciesList.forEach((currency) {
+      menu.add(Text(currency));
+    });
+    return CupertinoPicker(
+      itemExtent: 32.0,
+      backgroundColor: Colors.lightBlue,
+      onSelectedItemChanged: (selectedIndex) {
+        String selected = currenciesList[selectedIndex];
+        setState(() {
+          this.selectedCurrency = selected;
+          this.bitCoinPrice = '?';
+          this.ethreumPrice = '?';
+          this.lightCoinPrice = '?';
+        });
+        getData(selected);
+      },
+      children: menu,
+    );
+  }
+
+  DropdownButton<String> androidPicker() {
+    List<DropdownMenuItem<String>> menu = [];
+    currenciesList.forEach((currency) {
+      menu.add(DropdownMenuItem(child: Text(currency), value: currency));
+    });
+    return DropdownButton(
+      value: selectedCurrency,
+      items: menu,
+      onChanged: (value) {
+        setState(() {
+          this.selectedCurrency = value;
+          this.bitCoinPrice = '?';
+          this.ethreumPrice = '?';
+          this.lightCoinPrice = '?';
+        });
+        getData(value);
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    getData(this.selectedCurrency);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +95,49 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $bitCoinPrice $selectedCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 ETH = $ethreumPrice $selectedCurrency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 LTC = $lightCoinPrice $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -38,12 +148,11 @@ class _PriceScreenState extends State<PriceScreen> {
             ),
           ),
           Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
-          ),
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: Platform.isIOS ? iOSPicker() : androidPicker()),
         ],
       ),
     );
